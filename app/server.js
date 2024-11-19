3e20248a96d0c5b282be75846cc3d0776c49e2bd
Srinath <153373702+teja-srinath@users.noreply.github.com>
2024-11-19 14:14:57 +1100

    Create server.js
    node js server

---
+++ app/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World! Your app is deployed!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
